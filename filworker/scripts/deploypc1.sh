e4412139c047082e7e1b2dc1b61c6674898ffb76
FIL User <filuser@filnet.ddnsgeek.com>
2022-01-04 22:28:55 +0000

    fixing all the things

--- filworker/scripts/deploypc1.sh
+++ filworker/scripts/deploypc1.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-#trap "set +x; sleep 5; set -x" DEBUG
-set -x
-trap read debug
+#set -x
+#trap read debug
 
 # Create pc1 cgroup
 cgroupindex=1
 for f in /fil/calibnet/lotusworkers/pc1*; do
-  if [[ -d "$f" ]] && [[ -e "$f/.inuse"]]; then
+  if [[ -d "$f" ]] && [[ ! -a "$f/.inuse" ]] ; then 
     break
   fi
   ((cgroupindex++))
@@ -17,29 +16,46 @@ cgroup="pc1w$cgroupindex"
 # Create pc1 repo
 echo "Creating repo $cgroup."
 repo="/fil/calibnet/lotusworkers/$cgroup"
-mkdir -p $repo
-sudo mount --options size=4G -t tmpfs none $repo
-sudo chown -R filuser:fil $repo
-echo "Finished creating repo $cgroup."
+repoexists=$(ls -A $repo | wc -c)
+if [[ "$repoexists" == 0 ]] ; then
+  mkdir -p $repo
+  if sudo mount --options size=4G -t tmpfs none $repo ; then
+    sudo chown -R filuser:fil $repo
+    echo "Finished creating repo at $repo"
+  else
+    echo "Failed to mount ramdisk at $repo. Quitting..."
+    exit 1
+  fi
+fi
 
 # Create pc1 cgroup
-if [[ $(lscgroup cpuset:$cgroup) -ne 0 ]]; then
+cgroupexists=$(lscgroup cpuset:$cgroup | wc -c)
+if [[ "$cgroupexists" == 0 ]] ; then
   echo "Creating cgroup $cgroup."
-  let cpu=$i-1
-  let mem=0
-  sudo cgcreate -a filuser:fil -t filuser:fil -g cpuset:$cgroup
-  cgset -r cpuset.cpus=$cpu cpuset:$cgroup
-  cgset -r cpuset.mems=$mem cpuset:$cgroup
-  echo "Finished creating cgroup $cgroup."
+  let cpu=$cgroupindex-1
+  if sudo cgcreate -a filuser:fil -t filuser:fil -g cpuset:$cgroup ; then
+    cgset -r cpuset.cpus=$cpu $cgroup
+    cgset -r cpuset.mems=0 $cgroup
+    echo "Finished creating cgroup $cgroup."
+  else
+    echo "Failed to create cpuset:$cgroup. Quitting..."
+    exit 1
+  fi
 fi
 
 # Fit environment
 source ../.pc1.env
 
 # Deploy pc1 worker
-let port=3400+$i
+let port=3400+$cgroupindex
 listen="10.0.0.243:$port"
-cgexec -g cpuset:$cgroup --sticky nohup lotus-worker --worker-repo=$repo run --listen=$listen --precommit2=false --commit=false --timeout=0 > $repo/$cgroup.log 2>&1 &
-lotus-worker --worker-repo=$repo storage attach --seal /fil/sealing
-lotus-worker --worker-repo=$repo info
-touch $repo/.inuse
+if cgexec -g cpuset:$cgroup --sticky nohup lotus-worker --worker-repo=$repo run --listen=$listen --precommit2=false --commit=false --timeout=0 > $repo/$cgroup.log 2>&1 & ; then
+  lotus-worker --worker-repo=$repo storage attach --seal /fil/sealing
+  lotus-worker --worker-repo=$repo info
+  touch $repo/.inuse
+else
+  echo "Failed to start $cgroup. Quitting..."
+  exit 1
+fi
+
+exit 0
