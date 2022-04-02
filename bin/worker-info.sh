d127a6ab4f1e16b04bb1426957ac37d8e8d74ceb
Stephen Schwab <sdschwab772@gmail.com>
2022-04-02 19:37:38 +0000

    hello

--- bin/worker-info.sh
+++ bin/worker-info.sh
@@ -5,12 +5,4 @@ if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
 . "$dir/.utils.sh"
 
 _get_worker
-
-# Print worker info
 lotus-worker --worker-repo=$wrepopath info
-read -p "Press [enter] to continue..."
-
-# Print environment
-source $wrepopath/$wreponame.env
-pid=$(pgrep -f listen=0.0.0.0:$LOTUS_CUSTOM_PORT)
-strings /proc/$pid/environ
--- etc/profile.d/build.sh
+++ etc/profile.d/build.sh
@@ -4,3 +4,6 @@ export RUST_BACKTRACE=full
 export FFI_BUILD_FROM_SOURCE=1
 export FFI_USE_MULTICORE_SDR=1
 export GOLOG_LOG_LEVEL=info
+export FFI_USE_CUDA=1
+export BELLMAN_CUDA_NVCC_ARGS="--fatbin --gpu-architecture=sm_75 --generate-code=arch=compute_75,code=sm_75"
+export NEPTUNE_CUDA_NVCC_ARGS="--fatbin --gpu-architecture=sm_75 --generate-code=arch=compute_75,code=sm_75"
